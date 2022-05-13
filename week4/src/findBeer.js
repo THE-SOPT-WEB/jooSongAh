67d11acbbde3fd296b2c1cb3057d7ca2c5a83451
songah119 <skutcm119@gmail.com>
2022-05-13 19:54:21 +0900

    기본과제 완성!

---
+++ week4/src/findBeer.js
@@ -0,0 +1,123 @@
+import './findBeer.css';
+import {useRef,useState, useEffect} from "react";
+import axios from "axios"
+
+
+function FindBeer() {
+  const [beerBarList, setBeerBarList]=useState([]);
+  const [isCheck,setIsCheck]=useState(false); //near에 체크가 되어있는지 
+
+  async function 근처맥주집가져오기() {
+    const{x,y}=await 위치가져오기();
+
+    const result = await axios.get(
+      "https://dapi.kakao.com/v2/local/search/keyword?radius=20000",
+      {
+        headers: {
+          Authorization: `${process.env.REACT_APP_KAKAO_AK}`,
+        },
+        params: {
+          x: x,
+          y: y,
+          radius: 1000,
+          query: '맥주',
+        }
+      }
+    );
+    setBeerBarList(result.data.documents); //documents 객체 넣어주기 
+  }
+
+  async function 특정지역맥주집가져오기(location) {
+    const result = await axios.get(
+      "https://dapi.kakao.com/v2/local/search/keyword",
+      {
+        headers: {
+          Authorization: `${process.env.REACT_APP_KAKAO_AK}`,
+        },
+        params: {
+          query: location + " " + "맥주",
+        }
+      }
+  );
+  setBeerBarList(result.data.documents); //documents 객체 넣어주기 
+};
+
+  function handleClick(){ //검색버튼이 클릭되었을 경우 
+    if(isCheck){
+      근처맥주집가져오기();
+    }else{
+      특정지역맥주집가져오기(document.getElementById("textbox").value);
+    }
+  }
+
+  const getLocation = (errHandler) => {
+    if ("geolocation" in navigator) {
+            return new Promise((resolve) => {
+        navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const {
+            coords: { latitude: y, longitude: x },
+            } = position;
+            resolve({ x, y });
+        },
+        (e) => {
+            alert("HTTPS 연결을 확인해주세요.");
+            errHandler && errHandler();
+        }
+        );
+    });
+    } 
+  };
+
+  async function 위치가져오기() {
+    const result = await getLocation();
+    return result;
+  }
+  useEffect(() => {
+    console.log("beerBarList",beerBarList);
+  }, [beerBarList]);
+
+  function checkClickHandler (){
+    setIsCheck(prev=>!prev);
+  }
+
+  return (
+    <div className="find-beer">
+      <main>
+
+        <h1>🍺맥주를 갈겨보자🍺</h1>
+        <hr/>
+
+        <section className="search">
+          <form className="search--near-location">
+            <strong>지금 있는 곳에서 볼게요!</strong>
+            <input id="checkbox" type="checkbox" onClick={checkClickHandler}/>
+          </form>
+          <form className="search--input-location">
+            <strong>원하는 지역으로 볼게요!</strong>
+            <input id="textbox" type="text" placeholder='지역을 입력해주세요' disabled={isCheck} />
+          </form>
+          <button className="search-button" onClick={()=>handleClick()}>검색하기</button>
+        </section>
+
+        <hr/>
+        {beerBarList && beerBarList.map(({place_name, place_url, phone, distance, address_name }, idx) => {
+              return (
+                <article className="card" key={idx}>
+                  <p className="card--title" href={place_url || null}>{place_name}</p>
+                  <section className="card--detail">
+                    <p className="card--phone">{phone || "번호 없음"}</p>
+                    {isCheck && <p className="card--address">{distance}m</p>}
+                    {!isCheck&& <p className="card--address">{address_name}</p>}
+                  </section>
+                </article>
+              );
+            }
+          )}
+
+      </main>
+    </div>
+  );
+}
+
+export default FindBeer;
